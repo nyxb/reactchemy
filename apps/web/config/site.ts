c7ee2bfe896d76abd3f572adcab44682782bf2cf
nyxb <contact@nyxb.xyz>
2023-06-24 14:18:55 +0200

    ✨ feat(config): add marketing and site configuration files to the web app to improve SEO and user experience

---
+++ apps/web/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+  name: 'hookchemy',
+  description: 'React hook library, ready to use, written in Typescript.',
+  url: 'https://hookchemy.nyxb.zip',
+  ogImage:
+    'https://via.placeholder.com/1200x630.png/007ACC/fff/?text=hookchemy',
+  links: {
+    github: 'https://github.com/nyxb/hookchemy',
+    npm: 'https://www.npmjs.com/package/hookchemy',
+  },
+}
