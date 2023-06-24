76beaab4d2083b791e6a39078e883e4851d7dc36
nyxb <contact@nyxb.xyz>
2023-06-24 14:27:47 +0200

    ✨ feat(useRetryUntilResolved): add tests for useRetryUntilResolved hook ✨ feat(useScreen): add useScreen hook to get screen dimensions and create demo component to show its usage

---
+++ packages/hookchemy/src/hooks/useScreen/useScreen.demo.tsx
@@ -0,0 +1,14 @@
+import { useScreen } from '../../'
+
+export default function Component() {
+   const screen = useScreen()
+
+   return (
+    <div>
+      The current window dimensions are:{' '}
+      <code>
+        {JSON.stringify({ width: screen?.width, height: screen?.height })}
+      </code>
+    </div>
+   )
+}
