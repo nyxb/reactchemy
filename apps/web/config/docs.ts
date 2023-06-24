1394ac15a6901e55e828318b3957f61998fd11cf
nyxb <contact@nyxb.xyz>
2023-06-24 16:30:05 +0200

    🔧 chore(docs.ts): add type to NavItem import to improve code readability 🔧 chore(use-lock-body.ts): add indentation to improve code readability

--- apps/web/config/docs.ts
+++ apps/web/config/docs.ts
@@ -1,31 +1,31 @@
 import { getPosts } from '@/lib/mdx'
-import { DocsConfig, NavItem } from '@/types'
+import type { DocsConfig, NavItem } from '@/types'
 
 export const hookNavItems: NavItem[] = getPosts().map(post => ({
-  title: post.name,
-  href: post.href,
+   title: post.name,
+   href: post.href,
 }))
 
 export const docsConfig: DocsConfig = {
-  mainNav: [
-    {
-      title: 'Documentation',
-      href: '/introduction',
-    },
-  ],
-  sidebarNav: [
-    {
-      title: 'Getting Started',
-      items: [
-        {
-          title: 'Introduction',
-          href: '/introduction',
-        },
-      ],
-    },
-    {
-      title: 'Hooks',
-      items: hookNavItems,
-    },
-  ],
+   mainNav: [
+      {
+         title: 'Documentation',
+         href: '/introduction',
+      },
+   ],
+   sidebarNav: [
+      {
+         title: 'Getting Started',
+         items: [
+            {
+               title: 'Introduction',
+               href: '/introduction',
+            },
+         ],
+      },
+      {
+         title: 'Hooks',
+         items: hookNavItems,
+      },
+   ],
 }
