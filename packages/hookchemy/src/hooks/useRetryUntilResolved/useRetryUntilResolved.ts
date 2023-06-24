f383067d7868549f8504bf17c05f40f5dcc76d88
nyxb <contact@nyxb.xyz>
2023-06-24 14:27:34 +0200

    ✨ feat(useReadLocalStorage.ts): add hook to read from local storage and listen to changes ✨ feat(useRetryUntilResolved.ts): add hook to retry a function until it returns a truthy value

---
+++ packages/hookchemy/src/hooks/useRetryUntilResolved/useRetryUntilResolved.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { useInterval } from '../../'
+
+export function useRetryUntilResolved<T>(callback: () => T, interval = 100) {
+   const [hasResolved, setHasResolved] = useState(false)
+   useInterval(
+      () => {
+         const result = callback()
+         if (result)
+            setHasResolved(true)
+      },
+      hasResolved ? null : interval,
+   )
+   return hasResolved
+}
+export default useRetryUntilResolved
