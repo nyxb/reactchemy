c5d53af6df7368a438fb1a564645dbece32098b3
nyxb <contact@nyxb.xyz>
2023-06-24 14:29:30 +0200

    🚨 test(useUpdateEffect): add test to ensure callback function is called on update ✨ feat(useWindowSize): add useWindowSize hook to easily retrieve window dimensions and update onResize 📝 docs(useWindowSize): add description for useWindowSize hook 🚨 test(useWindowSize): add tests to ensure useWindowSize returns the correct dimensions 🚨 test(debounce): add test for debounce function

---
+++ packages/hookchemy/src/hooks/useWindowSize/useWindowSize.demo.tsx
@@ -0,0 +1,12 @@
+import { useWindowSize } from '../../'
+
+export default function Component() {
+   const { width, height } = useWindowSize()
+
+   return (
+    <div>
+      The current window dimensions are:{' '}
+      <code>{JSON.stringify({ width, height })}</code>
+    </div>
+   )
+}
