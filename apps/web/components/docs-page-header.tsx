44652deb4331b5024588d00492efde6806a32378
nyxb <contact@nyxb.xyz>
2023-06-24 16:28:21 +0200

    🎨 style(docs-page-header.tsx): fix indentation and quotes to match project's code style

--- apps/web/components/docs-page-header.tsx
+++ apps/web/components/docs-page-header.tsx
@@ -1,25 +1,25 @@
 import { cn } from '@/lib/utils'
 
 interface DocsPageHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
-  heading: string
-  text?: string
+   heading: string
+   text?: string
 }
 
 export function DocsPageHeader({
-  heading,
-  text,
-  className,
-  ...props
+   heading,
+   text,
+   className,
+   ...props
 }: DocsPageHeaderProps) {
-  return (
+   return (
     <>
       <div className={cn('space-y-4', className)} {...props}>
-        <h1 className="inline-block font-heading text-4xl lg:text-5xl">
+        <h1 className='inline-block font-heading text-4xl lg:text-5xl'>
           {heading}
         </h1>
-        {text && <p className="text-xl text-muted-foreground">{text}</p>}
+        {text && <p className='text-xl text-muted-foreground'>{text}</p>}
       </div>
-      <hr className="my-4" />
+      <hr className='my-4' />
     </>
-  )
+   )
 }
