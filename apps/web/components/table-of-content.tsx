1892a20dad80346443781e64582454146ba4e917
nyxb <contact@nyxb.xyz>
2023-06-24 16:29:47 +0200

    🔨 refactor(table-of-content.tsx): improve code readability and formatting 🔨 refactor(theme-provider.tsx): import type for ThemeProviderProps from next-themes to improve code quality

--- apps/web/components/table-of-content.tsx
+++ apps/web/components/table-of-content.tsx
@@ -7,120 +7,120 @@ import { useIsMounted } from 'hookchemy'
 import { cn } from '@/lib/utils'
 
 interface Item {
-  title: string
-  url: string
-  items?: Item[]
+   title: string
+   url: string
+   items?: Item[]
 }
 
 interface Items {
-  items?: Item[]
+   items?: Item[]
 }
 
 export type TableOfContents = Items
 
 interface TocProps {
-  toc: TableOfContents
+   toc: TableOfContents
 }
 
 export function DashboardTableOfContents({ toc }: TocProps) {
-  const itemIds = React.useMemo(
-    () =>
-      toc.items
-        ? toc.items
-            .flatMap(item => [item.url, item?.items?.map(item => item.url)])
-            .flat()
-            .filter(Boolean)
-            .map(id => id?.split('#')[1])
-        : [],
-    [toc],
-  )
-  const activeHeading = useActiveItem(itemIds)
-  const isMounted = useIsMounted()
+   const itemIds = React.useMemo(
+      () =>
+         toc.items
+            ? toc.items
+               .flatMap(item => [item.url, item?.items?.map(item => item.url)])
+               .flat()
+               .filter(Boolean)
+               .map(id => id?.split('#')[1])
+            : [],
+      [toc],
+   )
+   const activeHeading = useActiveItem(itemIds)
+   const isMounted = useIsMounted()
 
-  if (!toc?.items) {
-    return null
-  }
+   if (!toc?.items)
+      return null
 
-  return isMounted() ? (
-    <div className="space-y-2">
-      <p className="font-medium">On This Page</p>
+   return isMounted()
+      ? (
+    <div className='space-y-2'>
+      <p className='font-medium'>On This Page</p>
       <Tree tree={toc} activeItem={activeHeading} />
     </div>
-  ) : null
+         )
+      : null
 }
 
 function useActiveItem(itemIds: (string | undefined)[]) {
-  const [activeId, setActiveId] = React.useState<string>('')
+   const [activeId, setActiveId] = React.useState<string>('')
 
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            setActiveId(entry.target.id)
-          }
-        })
-      },
-      { rootMargin: `0% 0% -80% 0%` },
-    )
+   React.useEffect(() => {
+      const observer = new IntersectionObserver(
+         (entries) => {
+            entries.forEach((entry) => {
+               if (entry.isIntersecting)
+                  setActiveId(entry.target.id)
+            })
+         },
+         { rootMargin: '0% 0% -80% 0%' },
+      )
 
-    itemIds?.forEach(id => {
-      if (!id) {
-        return
-      }
+      itemIds?.forEach((id) => {
+         if (!id)
+            return
 
-      const element = document.getElementById(id)
-      if (element) {
-        observer.observe(element)
-      }
-    })
-
-    return () => {
-      itemIds?.forEach(id => {
-        if (!id) {
-          return
-        }
-
-        const element = document.getElementById(id)
-        if (element) {
-          observer.unobserve(element)
-        }
+         const element = document.getElementById(id)
+         if (element)
+            observer.observe(element)
       })
-    }
-  }, [itemIds])
 
-  return activeId
+      return () => {
+         itemIds?.forEach((id) => {
+            if (!id)
+               return
+
+            const element = document.getElementById(id)
+            if (element)
+               observer.unobserve(element)
+         })
+      }
+   }, [itemIds])
+
+   return activeId
 }
 
 interface TreeProps {
-  tree: TableOfContents
-  level?: number
-  activeItem?: string | null
+   tree: TableOfContents
+   level?: number
+   activeItem?: string | null
 }
 
 function Tree({ tree, level = 1, activeItem }: TreeProps) {
-  return tree?.items?.length && level < 3 ? (
+   return tree?.items?.length && level < 3
+      ? (
     <ul className={cn('m-0 list-none', { 'pl-4': level !== 1 })}>
       {tree.items.map((item, index) => {
-        return (
+         return (
           <li key={index} className={cn('mt-0 pt-2')}>
             <a
               href={item.url}
               className={cn(
-                'inline-block no-underline',
-                item.url === `#${activeItem}`
-                  ? 'font-medium text-primary'
-                  : 'text-sm text-muted-foreground',
+                 'inline-block no-underline',
+                 item.url === `#${activeItem}`
+                    ? 'font-medium text-primary'
+                    : 'text-sm text-muted-foreground',
               )}
             >
               {item.title}
             </a>
-            {item.items?.length ? (
+            {item.items?.length
+               ? (
               <Tree tree={item} level={level + 1} activeItem={activeItem} />
-            ) : null}
+                  )
+               : null}
           </li>
-        )
+         )
       })}
     </ul>
-  ) : null
+         )
+      : null
 }
--- apps/web/components/theme-provider.tsx
+++ apps/web/components/theme-provider.tsx
@@ -3,8 +3,8 @@
 import * as React from 'react'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
