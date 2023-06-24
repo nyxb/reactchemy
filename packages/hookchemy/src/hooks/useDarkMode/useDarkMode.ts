a2e7fc6ac15ee4ab5248c33211058d94c20e2da4
nyxb <contact@nyxb.xyz>
2023-06-24 14:21:07 +0200

    ✨ feat(useCounter): add useCounter hook to manage a counter state 🚨 test(useCounter): add tests for useCounter hook 📝 docs(useCounter): add documentation for useCounter hook ✨ feat(useDarkMode): add useDarkMode hook to manage dark mode state 🚨 test(useDarkMode): add tests for useDarkMode hook 📝 docs(useDarkMode): add documentation for useDarkMode hook ✨ feat(useDebounce): add useDebounce hook to debounce a value 📝 docs(useDebounce): add documentation for useDebounce hook 🎨 chore(useCounter, useDarkMode, useDebounce): format code and imports

---
+++ packages/hookchemy/src/hooks/useDarkMode/useDarkMode.ts
@@ -0,0 +1,30 @@
+import { useLocalStorage, useMediaQuery, useUpdateEffect } from '../../'
+
+const COLOR_SCHEME_QUERY = '(prefers-color-scheme: dark)'
+
+interface UseDarkModeOutput {
+   isDarkMode: boolean
+   toggle: () => void
+   enable: () => void
+   disable: () => void
+}
+
+export function useDarkMode(defaultValue?: boolean): UseDarkModeOutput {
+   const isDarkOS = useMediaQuery(COLOR_SCHEME_QUERY)
+   const [isDarkMode, setDarkMode] = useLocalStorage<boolean>(
+      'hookchemy-dark-mode',
+      defaultValue ?? isDarkOS ?? false,
+   )
+
+   // Update darkMode if os prefers changes
+   useUpdateEffect(() => {
+      setDarkMode(isDarkOS)
+   }, [isDarkOS])
+
+   return {
+      isDarkMode,
+      toggle: () => setDarkMode(prev => !prev),
+      enable: () => setDarkMode(true),
+      disable: () => setDarkMode(false),
+   }
+}
