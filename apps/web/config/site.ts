f744c142736ccabdcd0240c9479ad4e1efb3a74c
nyxb <contact@nyxb.xyz>
2023-06-26 09:22:46 +0200

    🎨 style(icons.tsx): add ChevronDown icon to the Icons object 📝 docs(site.ts): update site description to provide more information about the library and its features

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
    name: 'reactchemy',
-   description: 'React hook library, ready to use, written in Typescript.',
+   description: 'This robust library, written in TypeScript, includes a variety of ready-to-use Hooks and Components. It also incorporates a set of practical utilities, catering to the diverse needs of your development process. Whether you\'re swiftly prototyping or building a complex application, this library equips you with the necessary tools.',
    url: 'https://reactchemy.nyxb.zip',
    ogImage:
     'https://via.placeholder.com/1200x630.png/007ACC/fff/?text=reactchemy',
