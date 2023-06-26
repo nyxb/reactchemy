5ef97c849fe9f1cba6d162b5ef4f1f3e0b274034
nyxb <contact@nyxb.xyz>
2023-06-26 12:17:23 +0200

    🎨 style(page.tsx, layout.tsx, site.ts): replace placeholder image with actual og.png image to improve website aesthetics and branding

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -3,9 +3,9 @@ import type { SiteConfig } from '@/types'
 export const siteConfig: SiteConfig = {
    name: 'reactchemy',
    description: 'This robust library, written in TypeScript, includes a variety of ready-to-use Hooks and Components. It also incorporates a set of practical utilities, catering to the diverse needs of your development process. Whether you\'re swiftly prototyping or building a complex application, this library equips you with the necessary tools.',
-   url: 'https://reactchemy.nyxb.zip',
+   url: 'https://reactchemy.com',
    ogImage:
-    'https://via.placeholder.com/1200x630.png/007ACC/fff/?text=reactchemy',
+    '/og.png',
    links: {
       github: 'https://github.com/nyxb/reactchemy',
       npm: 'https://www.npmjs.com/package/reactchemy',
